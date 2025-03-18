8e7a4d765e05dc6a0f7f25bf9f6c60bea6d47f60
albukhari21 <albukhariinsankamil@gmail.com>
2025-03-18 12:25:53 +0700

    perulangan while

--- pertemuan 4/file1.cpp
+++ pertemuan 4/file1.cpp
@@ -17,3 +17,5 @@ int main(){
         cout << "Nilai pencacah terakhir = " << cacah;
     }
 }
+
+
--- pertemuan 4/file2.cpp
+++ pertemuan 4/file2.cpp
@@ -15,5 +15,17 @@ int main(){
     while(x < 7){
         cout << "nilai x = " << x << " Lari keliling lapangan!" << endl;
         x = rand() % 10;
+        perulanganWhile++;
     }
+    cout << "Nilai akhir x = " << x << endl;
+    cout << "Jumlah perulangan = " << perulanganWhile << endl;
+    cout << endl;
+    cout << "Perulangan dengan do while" << endl;
+    do{
+        cout << "nilai x = " << x << " Lari keliling lapangan!" << endl;
+        x = rand() % 10;
+        perulanganDoWhile++;
+    }while(x < 7);
+    cout << "Nilai akhir x = " << x << endl;
+    cout << "Jumlah perulangan = " << perulanganDoWhile << endl;
 }

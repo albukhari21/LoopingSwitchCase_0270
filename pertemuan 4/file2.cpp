558de2d8b65d217fa9ad58c692f429458331b6fc
albukhari21 <albukhariinsankamil@gmail.com>
2025-03-18 12:25:34 +0700

    new

---
+++ pertemuan 4/file2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int main(){
+    int x;
+    int perulanganWhile = 0;
+    int perulanganDoWhile = 0;
+    // Perulangan while
+    cout << "perulangan dengan while" << endl;
+    srand(time(0));
+    x = rand() % 10;
+    cout << "Nilai awal x = " << x << endl;
+    while(x < 7){
+        cout << "nilai x = " << x << " Lari keliling lapangan!" << endl;
+        x = rand() % 10;
+    }
+}
